0faaf0680a3ae9ee4108a8558787afd533cc41d9
devil <devil@gmail.com>
2025-08-05 23:47:52 +0530

    fix

--- controllers/doctor/auth.js
+++ controllers/doctor/auth.js
@@ -19,12 +19,12 @@ const login = async (req, res) => {
         if (!doctor) {
             return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "usernotfound"));
         }
-        // if (doctor.approveProfile === "Rejected") {
-        //     return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "profileRejected"));
-        // }
-        if (doctor.approveProfile === "Pending") {
-            return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "profilePending"));
+        if (doctor.approveProfile === "Rejected") {
+            return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "profileRejected"));
         }
+        // if (doctor.approveProfile === "Pending") {
+        //     return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "profilePending"));
+        // }
 
 
         const isPasswordValid = await bcrypt.compare(requestData.password, doctor.password);
@@ -61,7 +61,7 @@ const verifySignUpOTP = async (req, res) => {
             if (otp[0].otp === requestData.otp) {
                 let doctor = await doctorModel.findOne({ email: requestData.email.toLowerCase(), isDeleted: false, approveProfile: { $ne: "Rejected" } }).select("-password");
                 if (doctor.isProfileCompleted === true) return apiResponse.errorMessage(res, 400, CMS.Lang_Messages("en", "userexistemail"));
-                
+
                 console.log(doctor)
                 let payLoad = {
                     id: doctor._id,
