c591c26b4feed6580dcb184b4490bf750416b401
devil <devil@gmail.com>
2025-08-07 18:28:02 +0530

    add keys

--- controllers/socket/socket.controller.js
+++ controllers/socket/socket.controller.js
@@ -61,116 +61,134 @@ async function chatroomPagin(req, res) {
         }
         const basePipeline = [
             {
-            $lookup: {
-                from: "doctors",
-                localField: "doctorId",
-                foreignField: "_id",
-                as: "providerDetails"
-            }
+                $lookup: {
+                    from: "doctors",
+                    localField: "doctorId",
+                    foreignField: "_id",
+                    as: "providerDetails"
+                }
             },
             { $unwind: "$providerDetails" },
             {
-            $lookup: {
-                from: "images",
-                localField: "providerDetails.profileImage",
-                foreignField: "_id",
-                as: "providerDetails.profileImage"
-            }
+                $lookup: {
+                    from: "images",
+                    localField: "providerDetails.profileImage",
+                    foreignField: "_id",
+                    as: "providerDetails.profileImage"
+                }
             },
             { $unwind: { path: "$providerDetails.profileImage", preserveNullAndEmptyArrays: true } },
             {
-            $lookup: {
-                from: "patients",
-                localField: "patientId",
-                foreignField: "_id",
-                as: "patientDetails"
-            }
+                $lookup: {
+                    from: "patients",
+                    localField: "patientId",
+                    foreignField: "_id",
+                    as: "patientDetails"
+                }
             },
             { $unwind: { path: "$patientDetails", preserveNullAndEmptyArrays: true } },
             {
-            $lookup: {
-                from: "images",
-                localField: "patientDetails.profileImage",
-                foreignField: "_id",
-                as: "patientDetails.profileImage"
-            }
+                $lookup: {
+                    from: "images",
+                    localField: "patientDetails.profileImage",
+                    foreignField: "_id",
+                    as: "patientDetails.profileImage"
+                }
             },
             { $unwind: { path: "$patientDetails.profileImage", preserveNullAndEmptyArrays: true } },
             {
-            $match: {
-                ...matchStage,
-                ...(searchRegex && {
-                $or: req.doc.role === roles.doctor
-                    ? [
-                    { "patientDetails.name": { $regex: searchRegex } }
-                    ]
-                    : [
-                    { "providerDetails.name": { $regex: searchRegex } }
-                    ]
-                })
-            }
-            },
-            {
-            $lookup: {
-                from: "chats",
-                let: { roomId: "$_id" },
-                pipeline: [
-                {
-                    $match: {
-                    $expr: {
-                        $and: [
-                        { $eq: ["$chatRoom", "$$roomId"] },
-                        { $eq: ["$isSeen", false] },
-                        { $eq: ["$senderType", req.doc.role === roles.patient ? "provider" : "patient"] }
-                        ]
-                    }
-                    }
-                },
-                { $count: "unseenCount" }
-                ],
-                as: "unseenMessages"
-            }
-            },
-            {
-            $addFields: {
-                unseenMessageCount: {
-                $ifNull: [{ $arrayElemAt: ["$unseenMessages.unseenCount", 0] }, 0]
+                $match: {
+                    ...matchStage,
+                    ...(searchRegex && {
+                        $or: req.doc.role === roles.doctor
+                            ? [
+                                { "patientDetails.name": { $regex: searchRegex } }
+                            ]
+                            : [
+                                { "providerDetails.name": { $regex: searchRegex } }
+                            ]
+                    })
                 }
-            }
             },
             {
-            $unset: "unseenMessages"
-            }
+                $lookup: {
+                    from: "chats",
+                    let: { roomId: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ["$chatRoom", "$$roomId"] },
+                                        { $eq: ["$isSeen", false] },
+                                        { $eq: ["$senderType", req.doc.role === roles.patient ? "provider" : "patient"] }
+                                    ]
+                                }
+                            }
+                        },
+                        { $count: "unseenCount" }
+                    ],
+                    as: "unseenMessages"
+                }
+            },
+            {
+                $addFields: {
+                    unseenMessageCount: {
+                        $ifNull: [{ $arrayElemAt: ["$unseenMessages.unseenCount", 0] }, 0]
+                    }
+                }
+            },
+            {
+                $unset: "unseenMessages"
+            },
+            {
+                $lookup: {
+                    from: "services",
+                    localField: "serviceId",
+                    foreignField: "_id",
+                    as: "serviceId"
+                }
+            },
+            { $unwind: { path: "$serviceId", preserveNullAndEmptyArrays: true } }
         ];
 
         // Copy of pipeline for data + add pagination & project
         const dataPipeline = [
             ...basePipeline,
             {
-            $project: {
-                _id: 1,
-                doctorId: 1,
-                roomId: 1,
-                lastMessage: 1,
-                lastMessageAt: 1,
-                createdAt: 1,
-                chatRequestId: 1, // <-- Added this line
-                providerDetails: {
-                _id: 1,
-                name: 1,
-                profileImage: {
+                $project: {
                     _id: 1,
-                    path: 1
+                    doctorId: 1,
+                    serviceId: 1,
+                    isClosed: 1,
+                    roomId: 1,
+                    lastMessage: 1,
+                    lastMessageAt: 1,
+                    createdAt: 1,
+                    chatRequestId: 1, // <-- Added this line
+                    providerDetails: {
+                        _id: 1,
+                        name: 1,
+                        profileImage: {
+                            _id: 1,
+                            path: 1
+                        }
+                    },
+                    patientDetails: {
+                        _id: 1,
+                        ownerName: 1,
+                        name: 1,
+                        ownerGender: 1,
+                        ownerImage: {
+                            _id: 1,
+                            path: 1
+                        },
+                        color: 1,
+                        age: 1,
+                        breed: 1
+                    },
+                    unseenMessageCount: 1
                 }
-                },
-                patientDetails: {
-                _id: 1,
-                ownerName: 1,
-                name: 1,
-                ownerGender: 1,
-                },
-                unseenMessageCount: 1
-            }
             },
             { $sort: { lastMessageAt: -1 } },
             { $skip: startIndex },
